13e721052e0a8895c66df6db82326022edf8378b
cuongnguyen <cuongnnc2610@gmail.com>
2020-06-04 19:27:15 +0700

    [up] update css refactor code v2

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,56 +1,62 @@
-import { Recipe } from "./recipe.model";
-import { Injectable } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Subject } from "rxjs";
+import {Recipe} from "./recipe.model";
+import {Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
-  recipesChange = new Subject<Recipe[]>();
+    recipesChange = new Subject<Recipe[]>();
 
-  /* private recipes: Recipe[] = [
-    new Recipe(
-      "Tasty Schnitzel",
-      "A super-tasty Schnitzel - just awesome!",
-      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
-      [new Ingredient("Meat", 1), new Ingredient("French Fries", 2)]
-    ),
-    new Recipe(
-      "Big Fat Burger",
-      "What else you need to say?",
-      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
-      [new Ingredient("Buns", 1), new Ingredient("Meat", 2)]
-    )
-  ];
- */
-  private recipes: Recipe[] = [];
-  constructor(private slService: ShoppingListService) {}
+    /* private recipes: Recipe[] = [
+      new Recipe(
+        "Tasty Schnitzel",
+        "A super-tasty Schnitzel - just awesome!",
+        "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+        [new Ingredient("Meat", 1), new Ingredient("French Fries", 2)]
+      ),
+      new Recipe(
+        "Big Fat Burger",
+        "What else you need to say?",
+        "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+        [new Ingredient("Buns", 1), new Ingredient("Meat", 2)]
+      )
+    ];
+   */
+    private recipes: Recipe[] = [];
 
-  setRecipes(recipes: Recipe[]) {
-    this.recipes = recipes;
-    this.recipesChange.next(this.recipes.slice());
-  }
+    constructor(private slService: ShoppingListService) {
+    }
 
-  getRecipes() {
-    return this.recipes.slice();
-  }
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChange.next(this.recipes.slice());
+    }
 
-  addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
-  }
-  getRecipe(index: number) {
-    return this.recipes[index];
-  }
-  addRecipe(recipe: Recipe) {
-    this.recipes.push(recipe);
-    this.recipesChange.next(this.recipes.slice());
-  }
-  updateRecipe(index: number, newRecipe: Recipe) {
-    this.recipes[index] = newRecipe;
-    this.recipesChange.next(this.recipes.slice());
-  }
-  deleteRecipe(index: number) {
-    this.recipes.splice(index, 1);
-    this.recipesChange.next(this.recipes.slice());
-  }
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChange.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChange.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChange.next(this.recipes.slice());
+    }
 }
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,38 +1,39 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
 
-import { RecipesComponent } from "./recipes.component";
-import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
-import { RecipeResolverService } from "./recipes-resolver.service";
-import { AuthGuard } from "../auth/auth.guard";
+import {RecipesComponent} from "./recipes.component";
+import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {RecipeResolverService} from "./recipes-resolver.service";
+import {AuthGuard} from "../auth/auth.guard";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: "", component: RecipeStartComponent },
-      { path: "new", component: RecipeEditComponent },
-      {
-        path: ":id",
-        component: RecipeDetailComponent,
-        resolve: [RecipeResolverService]
-      },
-      {
-        path: ":id/edit",
-        component: RecipeEditComponent,
+    {
+        path: "",
+        component: RecipesComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {path: "", component: RecipeStartComponent},
+            {path: "new", component: RecipeEditComponent},
+            {
+                path: ":id",
+                component: RecipeDetailComponent,
+                resolve: [RecipeResolverService]
+            },
+            {
+                path: ":id/edit",
+                component: RecipeEditComponent,
 
-        resolve: [RecipeResolverService]
-      }
-    ]
-  }
+                resolve: [RecipeResolverService]
+            }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class RecipesRoutingModule {}
+export class RecipesRoutingModule {
+}
