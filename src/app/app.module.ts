ada0d7051775a4c82547d3d441f481be2738b505
nhinguyen <dddnhi@gmail.com>
2020-06-04 19:29:39 +0700

    [up] update css refactor code v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 
-import { HeaderComponent } from "./header/header.component";
-import { AlertComponent } from "./shared/alert/alert.component";
-import { SharedModule } from "./shared/shared.module";
-import { CoreModule } from "./core.module";
-import { LoggingService } from "./logging.service";
+import {HeaderComponent} from "./header/header.component";
+import {AlertComponent} from "./shared/alert/alert.component";
+import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
+import {LoggingService} from "./logging.service";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    SharedModule,
-    CoreModule
-  ],
-  bootstrap: [AppComponent],
-  // providers:[LoggingService],
-  entryComponents: [AlertComponent]
+    declarations: [AppComponent, HeaderComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        SharedModule,
+        CoreModule
+    ],
+    bootstrap: [AppComponent],
+    // providers:[LoggingService],
+    entryComponents: [AlertComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from "@angular/core";
 
 //@Injectable({ providedIn: "root" })
 export class LoggingService {
-  lastLog: string;
+    lastLog: string;
 
-  printLog(message: string) {
-    console.log(message);
-    console.log(this.lastLog);
-    this.lastLog = message;
-  }
+    printLog(message: string) {
+        console.log(message);
+        console.log(this.lastLog);
+        this.lastLog = message;
+    }
 }
